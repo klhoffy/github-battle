0d26eb03f9c7f69987ed1e59293f2914b902e2a6
Kristin Hoffer <kristinlhoffer@gmail.com>
2018-01-30 00:41:25 -0800

    update webpack babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './app/index.js',
@@ -12,4 +13,9 @@ module.exports = {
 			{ test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
 		]
 	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'app/index.html'
+		})
+	]
 }
